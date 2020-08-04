852d02a5871607cae72209dbd2bf98d3eb58f63a
Lee Cant <lee.cant@ampetronic.co>
2020-08-04 19:06:13 +0100

    Dependency injection and services

--- AngularGettingStarted/ClientApp/src/app/products/product-list.component.ts
+++ AngularGettingStarted/ClientApp/src/app/products/product-list.component.ts
@@ -1,21 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './iproduct';
+import { ProductService } from './product.service';
 
+//Product Service is transient via providers
 @Component({
+  //providers: [ProductService],
   selector: 'pm-products',
   styleUrls: ['./product-list.component.css'],
   templateUrl: './product-list.component.html'
 })
 
 export class ProductListComponent implements OnInit {
+  private _productService;
+
   filteredProducts: IProduct[];
   imageMargin: number = 2;
   imageWidth: number = 50;
 
   _listFilter: string;
+
   get listFilter(): string {
     return this._listFilter;
   }
+
   set listFilter(value: string) {
     this._listFilter = value;
     this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
@@ -25,36 +32,17 @@ export class ProductListComponent implements OnInit {
   showImage: boolean = false;
 
   //products normally call API
-  products: IProduct[] = [
-    {
-      "productId": 1,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2019",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "assets/images/leaf_rake.png"
-    },
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2019",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "assets/images/garden_cart.png"
-    }
-  ];
+  products: IProduct[];
 
-  constructor() {
-    this.filteredProducts = this.products;
-    this.listFilter = "cart";
+  //DI dont through the Constructor
+  //Private not necessary
+  constructor(private productService: ProductService) {
+
   }
 
   ngOnInit(): void {
-    console.log(`ProductListComponent initialised`);
+    this.products = this.productService.getproducts();
+    this.filteredProducts = this.products;
   }
 
   onRatingClicked(message: string): void {
---
+++ AngularGettingStarted/ClientApp/src/app/products/product.service.js
@@ -0,0 +1,77 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ProductService = void 0;
+var core_1 = require("@angular/core");
+//injection anywhere, Singleton
+var ProductService = /** @class */ (function () {
+    function ProductService() {
+    }
+    ProductService.prototype.getproducts = function () {
+        return [
+            {
+                "productId": 1,
+                "productName": "Leaf Rake",
+                "productCode": "GDN-0011",
+                "releaseDate": "March 19, 2019",
+                "description": "Leaf rake with 48-inch wooden handle.",
+                "price": 19.95,
+                "starRating": 3.2,
+                "imageUrl": "assets/images/leaf_rake.png"
+            },
+            {
+                "productId": 2,
+                "productName": "Garden Cart",
+                "productCode": "GDN-0023",
+                "releaseDate": "March 18, 2019",
+                "description": "15 gallon capacity rolling garden cart",
+                "price": 32.99,
+                "starRating": 4.2,
+                "imageUrl": "assets/images/garden_cart.png"
+            },
+            {
+                "productId": 5,
+                "productName": "Hammer",
+                "productCode": "TBX-0048",
+                "releaseDate": "May 21, 2019",
+                "description": "Curved claw steel hammer",
+                "price": 8.9,
+                "starRating": 4.8,
+                "imageUrl": "assets/images/hammer.png"
+            },
+            {
+                "productId": 8,
+                "productName": "Saw",
+                "productCode": "TBX-0022",
+                "releaseDate": "May 15, 2019",
+                "description": "15-inch steel blade hand saw",
+                "price": 11.55,
+                "starRating": 3.7,
+                "imageUrl": "assets/images/saw.png"
+            },
+            {
+                "productId": 10,
+                "productName": "Video Game Controller",
+                "productCode": "GMG-0042",
+                "releaseDate": "October 15, 2018",
+                "description": "Standard two-button video game controller",
+                "price": 35.95,
+                "starRating": 4.6,
+                "imageUrl": "assets/images/xbox-controller.png"
+            }
+        ];
+    };
+    ProductService = __decorate([
+        core_1.Injectable({
+            providedIn: "root"
+        })
+    ], ProductService);
+    return ProductService;
+}());
+exports.ProductService = ProductService;
+//# sourceMappingURL=product.service.js.map
